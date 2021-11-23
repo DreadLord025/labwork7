372c6a37d48be5e85d19283cd3b69599d0820956
DreadLord <kirill.shkolniy025@gmail.com>
2021-11-23 09:58:09 +0200

    OPI work

---
+++ OP(LAB7).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a = 5, y;
+	int urav;
+	int sum = 0;
+
+	cout << "Введите Y - ";
+	cin >> y;
+
+	for (int n = 1; n < 6; n++)
+	{
+		sum *= n;
+		cout << sum << endl;
+	}
+
+	urav = log(pow(y, 2)) + sum;
+	cout << "Z = " << urav << endl;
+	system("pause");
+	return 0;
+}
